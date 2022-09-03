5777e38401ace234fa656135c93f591db61b7646
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-09-03 19:57:08 -0400

    commit tree 92350eb98ad91de7f6fecc41b88412f718f7a10f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function matchingLetter(drivers, name) {
+    const answer = drivers.filter(drivers => drivers > [0]);
+    return answer
+}
